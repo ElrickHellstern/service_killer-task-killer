19482edb49bcecde3f3786a0eeb2e15639180e8e
ElrickHellstern <107644917+ElrickHellstern@users.noreply.github.com>
2022-07-06 18:10:26 -0500

    Update loyalty_remover.ps1

--- loyalty_remover.ps1
+++ loyalty_remover.ps1
@@ -4,3 +4,5 @@ schtasks /delete /tn "Loyalty Lane Restart" /f
 schtasks /delete /tn "Loyalty Lane PMT Updater" /f 
 schtasks /delete /tn "Loyalty Lane Rewards" /f
 schtasks /delete /tn "Loyalty Lane PMT Restarter" /f
+
+# Set-Service -Name "ConnectPOS - Server" -Status stopped -StartupType disabled; schtasks /delete /tn "Loyalty Lane Customer Updater" /f; schtasks /delete /tn "Loyalty Lane Restart" /f; schtasks /delete /tn "Loyalty Lane PMT Updater" /f; schtasks /delete /tn "Loyalty Lane Rewards" /f; schtasks /delete /tn "Loyalty Lane PMT Restarter" /f  :
